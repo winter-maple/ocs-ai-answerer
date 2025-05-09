90d9cd13d7cce7fccdb5b385d2dca98ab912591b
enncy <877526278@qq.com>
2025-05-09 20:55:03 +0800

    perf: 持续优化软件辅助安装提示

--- packages/scripts/src/utils/app.ts
+++ packages/scripts/src/utils/app.ts
@@ -4,14 +4,17 @@ export const $playwright = {
 	showError: () => {
 		const href = 'https://docs.ocsjs.com/docs/script-helper';
 		const errorEl = h('div', [
-			'软件辅助启动失败，无法执行脚本操作，请开启软件辅助后才能运行，点击链接查看开启教程 => ',
+			'当前页面需要下载OCS桌面端，并在桌面端中新建浏览器，在新建的浏览器中才能进行正常刷课，点击链接查看详情 => ',
 			h('a', { href: href, target: '_blank' }, href)
 		]);
 		$modal.alert({
 			maskCloseable: false,
 			title: '⛔ 错误',
-			confirmButtonText: '我已知晓',
-			content: errorEl.cloneNode(true)
+			confirmButtonText: '查看详情',
+			content: errorEl.cloneNode(true),
+			onConfirm() {
+				window.open(href, '_blank');
+			}
 		});
 		$message.error({ content: errorEl.cloneNode(true), duration: 0 });
 	}
