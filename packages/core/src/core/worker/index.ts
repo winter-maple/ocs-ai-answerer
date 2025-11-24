089aaafcfefcdb1ae69fde9cdb0e1ad7aed376d3
enncy <877526278@qq.com>
2025-11-25 02:12:05 +0800

    fix(core): 修复上个版本无法打包的BUG

--- packages/core/src/core/worker/index.ts
+++ packages/core/src/core/worker/index.ts
@@ -1,4 +1,4 @@
 export * from './interface';
 export * from './utils';
 export { OCSWorker } from './worker';
-export { defaultQuestionResolve } from './question.resolver';
+export { createDefaultQuestionResolver } from './question.resolver';
